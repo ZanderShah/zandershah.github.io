c24ea8ec7d3c5c03774dbeeabba32f4ebe88f225
ZanderShah <alexander.shah@uwaterloo.ca>
2017-08-17 00:19:44 -0400

    post rest

--- server/app.js
+++ server/app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser')
 
 const app = express();
-
-// Serve static assets
+app.use(bodyParser.json());
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
-// Always return the main index.html, so react-router render the route in the client
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+});
+
+app.post('/summoners-war/run', (req, res) => {
+  var ret = {
+    message: 'Hello There',
+  }
+  res.status(200).json(ret);
 });
 
 module.exports = app;
