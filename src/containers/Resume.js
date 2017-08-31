6e56d117c310eca39fbfc81552fc166dbf5157e0
ZanderShah <alexander.shah@uwaterloo.ca>
2017-08-30 21:20:42 -0400

    spacing and sizing

--- src/containers/Resume.js
+++ src/containers/Resume.js
@@ -4,8 +4,8 @@ import { FaFacebook, FaGithub, FaTwitter, FaLinkedin, FaFileTextO } from 'react-
 import PDF from '../resume.pdf';
 
 const tight = {
-  marginTop: '5px',
-  marginBottom: '5px',
+  marginTop: 0,
+  marginBottom: 0,
   padding: 0,
 }
 
@@ -13,10 +13,10 @@ class Resume extends Component {
   render() {
     return (
       <div style={{textAlign: 'center', paddingTop: '30vh'}}> 
-        <h1 style={tight}>Alexander Shah</h1>
-        <h3 style={tight}>Software Engineering</h3>
-        <hr style={{...tight, ...{width: '10px', border: '1px solid indianred'}}}/>
-        <h5 style={tight}>University of Waterloo</h5>
+        <h1 style={{...tight, ...{fontSize: '5.5vh'}}}>Alexander Shah</h1>
+        <h3 style={{...tight, ...{fontSize: '3vh'}}}>Software Engineering</h3>
+        <hr style={{...tight, ...{width: '10px', border: '1px solid indianred', marginTop: '5px', marginBottom: '5px'}}}/>
+        <h5 style={{...tight, ...{fontSize: '2vh', marginBottom: '10px'}}}>University of Waterloo</h5>
 
         <IconHoverable link='https://www.github.com/zandershah'><FaGithub size='42px' /></IconHoverable>
         <IconHoverable link='https://twitter.com/Zander_Shah'><FaTwitter size='42px' /></IconHoverable>
